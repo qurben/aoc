d533ed73d32cb9c5cdcacc13b08a585ece739ddb
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-12-12 16:34:53 +0100

    [2020] day 12

--- 2020/day12.js
+++ 2020/day12.js
@@ -1,36 +1,39 @@
 {
-    const instrs = document.querySelector("pre").textContent.trim().split("\n")
-        .map(l => [l.substr(0, 1), Number(l.substr(1))])
-
     const dirs = ["N", "E", "S", "W"]
 
-    const [, x, y] = instrs.reduce(([currentDir, x, y], [dir, distance]) => ({
-        N: [currentDir, x, y + distance],
-        S: [currentDir, x, y - distance],
-        E: [currentDir, x + distance, y],
-        W: [currentDir, x - distance, y],
-        L: [dirs[(dirs.indexOf(currentDir) + (3 * distance / 90)) % 4], x, y],
-        R: [dirs[(dirs.indexOf(currentDir) + (1 * distance / 90)) % 4], x, y],
-    }[dir == "F" ? currentDir : dir]), ["E", 0, 0])
-
-    console.log("Part 1:", Math.abs(x) + Math.abs(y))
+    console.log("Part 1:", document
+        .querySelector("pre")
+        .textContent
+        .trim()
+        .split("\n")
+        .map(l => [l.substr(0, 1), Number(l.substr(1))])
+        .reduce(([cd, x, y], [c, d]) => ({
+            N: [cd, x, y + d],
+            S: [cd, x, y - d],
+            E: [cd, x + d, y],
+            W: [cd, x - d, y],
+            L: [dirs[(dirs.indexOf(cd) + 3 * d / 90) % 4], x, y],
+            R: [dirs[(dirs.indexOf(cd) + 1 * d / 90) % 4], x, y],
+        }[c == "F" ? cd : c]), ["E", 0, 0])
+        .slice(1)
+        .reduce((sum, n) => sum + Math.abs(n), 0))
 }
 
 {
-    const instrs = document.querySelector("pre").textContent.trim().split("\n")
+    console.log("Part 2:", document
+        .querySelector("pre")
+        .textContent
+        .trim()
+        .split("\n")
         .map(l => [l.substr(0, 1), Number(l.substr(1))])
-
-    const rotate = (deg, wx, wy) => ({ 90: [wy, -wx], 180: [-wx, -wy], 270: [-wy, wx] }[deg])
-
-    const [x, y] = instrs.reduce(([x, y, wx, wy], [dir, distance]) => ({
-        N: () => [x, y, wx, wy + distance],
-        S: () => [x, y, wx, wy - distance],
-        E: () => [x, y, wx + distance, wy],
-        W: () => [x, y, wx - distance, wy],
-        R: () => [x, y, ...rotate(distance, wx, wy)],
-        L: () => [x, y, ...rotate(360 - distance, wx, wy)],
-        F: () => [x + distance * wx, y + distance * wy, wx, wy],
-    }[dir]()), [0, 0, 10, 1])
-
-    console.log("Part 2:", Math.abs(x) + Math.abs(y))
+        .reduce(([x, y, dx, dy], [c, d]) => ({
+            N: () => [x, y, dx, dy + d],
+            S: () => [x, y, dx, dy - d],
+            E: () => [x, y, dx + d, dy],
+            W: () => [x, y, dx - d, dy],
+            R: () => [x, y, ...{ 90: [dy, -dx], 180: [-dx, -dy], 270: [-dy, dx] }[d]],
+            L: () => [x, y, ...{ 90: [-dy, dx], 180: [-dx, -dy], 270: [dy, -dx] }[d]],
+            F: () => [x + d * dx, y + d * dy, dx, dy],
+        }[c]()), [0, 0, 10, 1])
+        .reduce((sum, n) => sum + Math.abs(n), 0))
 }
