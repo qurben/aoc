a41d21f982f76f040ef0754276051088962db82a
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-12-16 21:37:57 +0100

    [2020] Update day 16

--- 2020/day16.js
+++ 2020/day16.js
@@ -2,46 +2,37 @@
     const [constraintsInput, myInput, nearbyInput] = document.body.firstChild.textContent.trim().split("\n\n")
 
     const constraints = constraintsInput.split("\n")
-        .map(l => l.match(/(.+): (.+)-(.+) or (.+)-(.+)/))
+        .map(_ => _.match(/(.+): (.+)-(.+) or (.+)-(.+)/))
         .map(([, name, x1, x2, x3, x4]) => [name, Number(x1), Number(x2), Number(x3), Number(x4)])
 
-    const checkConstraints = (v, c) => c.filter(([name, from1, to1, from2, to2]) => {
-        return (v >= from1 && v <= to1) || (v >= from2 && v <= to2)
-    })
+    const check = (v, c) => c.filter(([name, from1, to1, from2, to2]) => (v >= from1 && v <= to1) || (v >= from2 && v <= to2))
 
-    const my = myInput.split("\n").slice(1).map(l => l.split(",").map(Number))[0]
+    const my = myInput.split("\n").slice(1).map(_ => _.split(",").map(Number))[0]
 
-    const nearby = nearbyInput.split("\n").slice(1).map(l => l.split(",").map(Number))
+    const nearby = nearbyInput.split("\n").slice(1).map(_ => _.split(",").map(Number))
 
-    console.log("Part 1:", nearby.map(n => n.filter(v => checkConstraints(v, constraints).length == 0)).flatMap(n => n).reduce((a, b) => a + b, 0))
+    console.log("Part 1:", nearby.map(_ => _.filter(v => check(v, constraints).length == 0)).flatMap(_ => _).reduce((a, b) => a + b, 0))
 
-    const nearbyValid = nearby.filter(n => n.every(v => checkConstraints(v, constraints).length > 0))
-
-    let index = 0
+    const nearbyValid = nearby.filter(_ => _.every(v => check(v, constraints).length > 0))
 
     const foundConstraints = []
 
-    let constraintsToCheck = constraints.slice()
-
-    let intersection = (a, b) => new Set(
-        [...a].filter(x => b.has(x)))
-
     for (let i = 0; i < nearbyValid[0].length; i++) {
-        foundConstraints.push(
-            nearbyValid.map(n => checkConstraints(n[i], constraintsToCheck).map(([n]) => n))
-                .reduce((acc, n) => acc ? intersection(acc, new Set(n)) : new Set(n)))
+        foundConstraints.push(nearbyValid
+            .map(n => check(n[i], constraints).map(([n]) => n))
+            .reduce((acc, n) => acc ? acc.filter(x => n.includes(x)) : n)
+        )
     }
 
-    while (foundConstraints.every(_ => _.size == 1) != true) {
-        const satisfied = foundConstraints.filter(_ => _.size == 1).map(a => [...a]).flatMap(_ => _)
+    while (!foundConstraints.every(_ => _.length == 1)) {
+        const satisfied = foundConstraints.filter(_ => _.length == 1).flatMap(_ => _)
 
-        const next = foundConstraints.slice().sort((a, b) => a.size - b.size).find(_ => _.size > 1)
+        const next = foundConstraints.slice().sort((a, b) => a.length - b.length).find(_ => _.length > 1)
 
-        satisfied.forEach(v => next.delete(v))
+        satisfied.forEach(v => next.splice(next.indexOf(v), 1))
     }
 
     console.log("Part 2:", foundConstraints
-        .map(a => [...a])
         .flatMap(_ => _)
         .map((c, i) => c.startsWith("departure") ? my[i] : 1)
         .reduce((a, b) => a * b, 1))
