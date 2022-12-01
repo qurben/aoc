cb598dbd3ad3fc09ebe6593dbdadec47b1430830
Gerben Oolbekkink <qurben@users.noreply.github.com>
2022-12-01 09:54:49 +0100

    Update day1.js

--- 2022/day1.js
+++ 2022/day1.js
@@ -1,8 +1,10 @@
 {
-  let totals = document.body.firstChild.textContent.trim().split("\n")
-    .reduce(([head, ...tail], v) => v == "" ? [0, head, ...tail] : [head + +v, ...tail], [])
+  let totals = n => document.body.firstChild.textContent.trim().split("\n")
+    .reduce(([head, ...tail], v) => v ? [head + +v, ...tail] : [0, head, ...tail], [])
     .sort((a, b) => b - a)
+  	.splice(0, n)
+  	.reduce((a, b) => a + b)
 
-  console.log("Day 1 Part 1: ", totals[0])
-  console.log("Day 1 Part 2: ", totals.splice(0, 3).reduce((a, b) => a + b))
+  console.log("Day 1 Part 1: ", totals(1))
+  console.log("Day 1 Part 2: ", totals(3))
 }
