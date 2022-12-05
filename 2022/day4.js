f4975106358c3f5a29adf5f0b64a9995bca95371
Gerben Oolbekkink <qurben@users.noreply.github.com>
2022-12-05 11:43:07 +0100

    [2022] Update day4.js

--- 2022/day4.js
+++ 2022/day4.js
@@ -1,40 +1,12 @@
 {
   let items = document.firstChild.textContent.trim().split("\n")
-  	.map(l => l.split(",").map(v => v.split("-").map(Number)))
-  
-  let contains = ([[a,b], [c,d]]) => {
-    if (a >= c && b <= d) {
-      return true;
-    }
-    
-    if (c >= a && d <= b) {
-      return true;
-    }
-    
-    return false;
-  }
-    
-  console.log(items.map(contains).reduce((a,b) => a + b, 0))
-  
-  let overlap = ([[a,b], [c,d]]) => {
-    if (b >= c && b <= d) {
-      return true;
-    }
-    
-    if (a >= c && a <= d) {
-      return true;
-    }
-    
-    if (c >= a && c <= b) {
-      return true;
-    }
-    
-    if (d >= a && d <= b) {
-      return true;
-    }
-    
-    return false;
-  }
-  
-    console.log(items.map(overlap).reduce((a,b) => a + b, 0))
+    .map(l => l.split(",").map(v => v.split("-").map(Number)))
+
+  let add = (a, b) => a + b
+
+  let contains = ([[a, b], [c, d]]) => (a >= c && b <= d) || (c >= a && d <= b)
+  let overlap = ([[a, b], [c, d]]) => !((b < c && b < d) || (a > c && a > d))
+
+  console.log("Day 4 part 1:", items.map(contains).reduce(add))
+  console.log("Day 4 part 2:", items.map(overlap).reduce(add))
 }
